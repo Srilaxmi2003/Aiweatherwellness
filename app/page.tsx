582fcf0a4a32e627584f1fbaff81a1cfad0114aa
Srilaxmi2003 <117028621+Srilaxmi2003@users.noreply.github.com>
2024-07-06 23:29:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
             target="_blank"
             className=" text-green-300 font-bold"
           >
-            SriLaxmi
+            Srilaxmi
           </a>
         </p>
       </footer>
