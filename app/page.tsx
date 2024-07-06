ad36f317f57f67285bb3ea32148fc611309f7759
Kumarikajal25 <kumarinaina9876@gmail.com>
2024-07-06 21:39:29 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,118 @@
+"use client"
+
+import { Spotlight } from "@/components/ui/Spotlight";
+import AirPollution from "./CreatedComponent/AirPollution/AirPollution";
+import Navbar from "./CreatedComponent/Navbar";
+import Temperature from "./CreatedComponent/Temperature/Temperature";
+import Sun from "./CreatedComponent/Sun/Sun";
+import Wind from "./CreatedComponent/wind/Wind";
+import DailyForecast from "./CreatedComponent/DailyForecast/DailyForecast";
+import Uvindex from "./CreatedComponent/Uvindex/Uvindex";
+import Population from "./CreatedComponent/Population/Population";
+import FeelsLike from "./CreatedComponent/FeelsLike/FeelsLike";
+import Humidity from "./CreatedComponent/Humidity/Humidity";
+import Visibility from "./CreatedComponent/Visibility/Visibility";
+import Pressure from "./CreatedComponent/Pressure/Pressure";
+import FiveDayForecast from "./CreatedComponent/FiveDayForecast/fiveDayForecast";
+import defaultStates from "./utils/defaultStates";
+import { useGlobalContextUpdate } from "@/app/ContextApi/GlobalContext";
 import Image from "next/image";
+import dynamic from 'next/dynamic';
+const DynamicMapbox = dynamic(() => import('./CreatedComponent/Map/Map'), {
+  ssr: false
+});
 
 export default function Home() {
+
+  const { setActiveCityCoords } = useGlobalContextUpdate();
+
+  const getClickedCityCords = (lat: number, lon: number) => {
+    setActiveCityCoords([lat, lon]);
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+ 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=" min-h-screen">
+      
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+    <div className=" h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.04]    ">
+     
+    <div className=" mx-[3rem] lg:mx-[3rem]  xl:mx-[4rem] 2xl:mx-[10rem] m-auto">
+      <Navbar/>
+      <div className=" pb-4 flex flex-col gap-4 md:flex-row">
+        {/* left-section */}
+
+        <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[32rem]">
+       <Temperature/>
+      <FiveDayForecast/>
+          </div>
+          {/* Right-section */}
+        <div className="flex flex-col  w-full">
+          <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+            <AirPollution/>
+            <Sun/>
+           <Wind/>
+            <DailyForecast/>
+            <Uvindex/>
+            <Population/>
+            <FeelsLike/>
+            <Humidity/>
+            <Visibility/>
+            <Pressure/>
+          </div>
+           
+
+            <div className="mapbox-con mt-4 flex gap-4">
+            <DynamicMapbox/>
+            <div className="states flex flex-col gap-3 flex-1">
+              <h2 className="flex items-center gap-2 font-medium">
+                Top Large Cities
+              </h2>
+              <div className="flex flex-col gap-4">
+                {defaultStates.map((state, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="border rounded-lg cursor-pointer dark:bg-dark-grey  dark:shadow-none  shadow-lg bg-zinc-100  hover:border-slate-600     dark:hover:border-zinc-600"
+                      onClick={() => {
+                        getClickedCityCords(state.lat, state.lon);
+                      }}
+                    >
+                      <p className="px-6 py-4">{state.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
+
+
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    </div>
+    
+    </div>
+    <footer className="py-4 flex  justify-start sm:justify-center   pb-5 sm:pb-8">
+        <p className="footer-text text-sm flex items-center gap-1  pl-3  sm:pl-0">
+          Made by
+          <Image 
+          src={"/logo-white.svg"} alt="logo" width={20} height={20} />
+          <a
+           
+            target="_blank"
+            className=" text-green-300 font-bold"
+          >
+            Kajal
+          </a>
+        </p>
+      </footer>
     </main>
   );
 }
